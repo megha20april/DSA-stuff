9551639cede4d895d5ff8f1463b8e9a30689bbac
Megha Agrawal <110454862+megha20april@users.noreply.github.com>
2024-08-12 06:43:36 +0530

    Create 141. Linked List Cycle.java

---
+++ leetcode/Linked-List/141. Linked List Cycle.java
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/linked-list-cycle/description/
+// Easy
+
+// Time and space both linear
+// it's not a very good solution but honestly i find it very cool, 
+// since we're storing references of each node in arraylist
+// and then nextly checking if the next node is already present in the arraylist
+// that would tell us we've already traversed through it once
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ArrayList<String> ls = new ArrayList<>();
+
+        ListNode temp = head;
+        while(temp != null){
+            ls.add(temp.toString());
+            if(temp.next != null && ls.contains(temp.next.toString())) return true;
+            temp = temp.next;
+        }
+
+        return false;
+    }
+}
+
+//=============Solution 2=============
+
+// time complexity is O(n)
+// This is because in the worst case scenario, the fast pointer will traverse the entire linked list once before detecting a cycle.
+
+// Constant space complexity
+
+// this is my second and better approach
+// basically i here, tried to see if i could forcefully do something with two pointers
+// since i've seen it getting used so much in linked list ques
+// here i basically returned true when slow == fast
+// if there is a cycle, the fast pointer will eventually catch up to the slow pointer. 
+// If there is no cycle, the fast pointer will reach the end of the list.
+
+// but there's one problem with this, i.e. initially with slow == fast == head
+// it would always return true
+// so i had to first move them forward and then check
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+
+        return false;
+    }
+}
