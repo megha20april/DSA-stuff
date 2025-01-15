cddb6e34cd51573fcce12bcb2132830ac3c79b70
Megha Agrawal <110454862+megha20april@users.noreply.github.com>
2025-01-15 09:58:26 +0530

    Update 26. Remove Duplicates from Sorted Array.java

--- leetcode/Arrays/Two Pointers/26. Remove Duplicates from Sorted Array.java
+++ leetcode/Arrays/Two Pointers/26. Remove Duplicates from Sorted Array.java
@@ -5,13 +5,20 @@
 
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int no = 1;
+        int nonDuplicate = 1; // this is already one since index 0th element will be smallest hence, shouldn't be interfered with. So, we've already conunted it
+
+        // and since the 0th index already has the smallest element, we start from index 1.
         for(int i = 1; i < nums.length; i++){
+            // since, we started from index 1, we compare the element with its predecessor
             if(nums[i] != nums[i-1]){
-                nums[no] = nums[i]; // swap with the last non-duplicate no.'s place
-                no++; // this will only move when there's a non-duplicate number
+                // for example, nonDuplicate is 1, and when we get a non-duplicate no. it becomes 2
+                // this is telling us that this non-duplicate element can take the 2nd positon in the array
+                // since it's the second unique no.
+                
+                nonDuplicate++; // this will only move when there's a non-duplicate number
+                nums[nonDuplicate - 1] = nums[i]; // swap with the last non-duplicate no.'s place
             }
         }
-        return no;
+        return nonDuplicate;
     }
 }
